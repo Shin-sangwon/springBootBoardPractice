ed2a84d0483cc476bb2d3663342c2d861a614ff7
Shin-sangwon <dnjsml10@gmail.com>
2022-08-08 11:39:46 +0900

    Controller, Mapping, ResponseBody 추가

---
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -0,0 +1,18 @@
+package com.ll.exam.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        // 서버
+        System.out.println("hi");
+        // 브라우저
+        return "hello";
+    }
+}
