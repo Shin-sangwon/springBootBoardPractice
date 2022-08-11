efa3bba60b2cc9d5dc38e935a7340de61108e2f0
Shin-sangwon <dnjsml10@gmail.com>
2022-08-11 10:55:25 +0900

    mariaDB 적용

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -48,14 +48,14 @@ class SbbApplicationTests {
 		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
 
 
-		assertEquals(5, q.getId());
+		assertEquals(1, q.getId());
 	}
 
 	@Test
 	void testJpa4() {
 		Question q = this.questionRepository.findBySubjectAndContent(
 				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(5, q.getId());
+		assertEquals(1, q.getId());
 	}
 
 	@Test
@@ -67,11 +67,45 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa6() {
-		Optional<Question> oq = this.questionRepository.findById(5);
+		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		q.setSubject("수정된 제목");
 		this.questionRepository.save(q);
+	}
+
+	@Test
+	void testJpa7() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Test
+	void testJpa8() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+
+	@Test
+	void testJpa9() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
 	}
 
 }
